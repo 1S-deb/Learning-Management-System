02e692c30c479e3001b7cc5cc74485bdcaeb5a68
1S-deb <srinjanadeb@gmail.com>
2025-03-08 11:04:59 +0530

    corrected user creation

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -12,13 +12,13 @@ export const clerkWebhooks=async(req,res)=>
             "svix-signature":req.headers["svix-signature"]
         })
         const {data,type}=req.body
-        switch(key)
+        switch(type)
         {
             case "user.created":
                 {
                     const userData={
                         _id:data.id,
-                        email:data.email_addresses[0].email_address,
+                        email:data.email_address[0].email_address,
                         name:data.first_name + " " + data.last_name,
                         imageUrl :data.image_url,
                     }
