11f7faf985fd2300b28faf38077cc90523b95c6d
1S-deb <srinjanadeb@gmail.com>
2025-03-11 11:29:25 +0530

    stripe added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,13 +3,15 @@ import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
 import React from "react";
+import {useAuth,useUser} from "@clerk/clerk-react";
 export const AppContext = createContext()
 
 export const AppContextProvider = (props)=>{
 
   const currency = import.meta.env.VITE_CURRENCY
   const navigate = useNavigate()
-
+  const {getToken}= useAuth()
+   const {user}= useUser()
   const [allCourses, setAllCourses] = useState([])
   const [isEducator, setIsEducator] = useState(true)
   const [enrolledCourses, setEnrolledCourses] = useState([])
@@ -68,7 +70,18 @@ export const AppContextProvider = (props)=>{
     fetchAllCourses()
     fetchUserEnrolledCourses()
   },[])
-  
+  const logToken =async()=>
+  {
+     console.log(await getToken());
+  };
+  useEffect(()=>
+  {
+    if(user)
+      {
+        logToken()
+      }
+  },[user]);
+
   const value = {
     currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, calculateNoOfLectures, calculateCourseDuration, calculateChapterTime, enrolledCourses, fetchUserEnrolledCourses
   }
--- server/models/User.js
+++ server/models/User.js
@@ -14,5 +14,5 @@ const userSchema = new mongoose.Schema(
         ],
 
     },{timestamps:true});
- const User= mongoose.model('User',userSchema);
-    export default User
+const User= mongoose.model('User',userSchema);
+export default User
